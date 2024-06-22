1d0771b9e6c9faa14005bb8a7c7829fc2b06e7ad
ccrs <ccrs@users.noreply.github.com>
2024-06-22 13:05:51 +0200

    heeeeeeeeeeeellooooooooo

--- src/server/game/Battlefield/Battlefield.cpp
+++ src/server/game/Battlefield/Battlefield.cpp
@@ -16,7 +16,83 @@
  */
 
 #include "Battlefield.h"
-#include "BattlefieldMgr.h"
-#include "BattlegroundPackets.h"
+#include "BattlefieldEntities.h"
 
+Battlefield::Battlefield(BattlefieldBattleId battleId, BattlefieldZoneId zoneId) : _battleId(battleId), _zoneId(zoneId), _enabled(false), _active(false), _controllingTeam(PVP_TEAM_NEUTRAL), _timer(0)
+{
+}
 
+Battlefield::~Battlefield()
+{
+}
+
+bool Battlefield::Initialize(bool enabled)
+{
+    _enabled = enabled;
+    return true;
+}
+
+void Battlefield::Update(uint32 /*diff*/)
+{
+}
+
+void Battlefield::HandlePlayerEnterZone(Player* /*player*/)
+{
+}
+
+void Battlefield::HandlePlayerLeaveZone(Player* /*player*/)
+{
+}
+
+void Battlefield::HandleAreaSpiritHealerQueryOpcode(Player* /*player*/, ObjectGuid /*source*/)
+{
+}
+
+void Battlefield::HandleAddPlayerToResurrectionQueue(Player* /*player*/, ObjectGuid /*source*/)
+{
+}
+
+void Battlefield::HandleRemovePlayerFromResurrectionQueue(Player* /*player*/)
+{
+}
+
+void Battlefield::EmplaceGraveyard(uint8 id, BattlefieldGraveyardPointer&& pointer)
+{
+    _graveyards.emplace(id, std::move(pointer));
+}
+
+Battlefield::BattlefieldGraveyardPointer& Battlefield::GetGraveyard(uint8 graveyardId)
+{
+    return _graveyards[graveyardId];
+}
+
+PvPTeamId Battlefield::GetAttackingTeam() const
+{
+    switch (GetControllingTeam())
+    {
+        case PVP_TEAM_HORDE:
+            return PVP_TEAM_ALLIANCE;
+        case PVP_TEAM_ALLIANCE:
+            return PVP_TEAM_HORDE;
+        case PVP_TEAM_NEUTRAL:
+            return PVP_TEAM_NEUTRAL;
+        default:
+            break;
+    }
+    return PVP_TEAM_NEUTRAL;
+}
+
+TeamId Battlefield::GetControllingTeamId() const
+{
+    return TeamIdByPvPTeamId(GetControllingTeam());
+}
+
+TeamId Battlefield::GetAttackingTeamId() const
+{
+    return TeamIdByPvPTeamId(GetAttackingTeam());
+}
+
+WorldSafeLocsEntry const* Battlefield::GetClosestGraveyardLocation(Player* /*who*/) const
+{
+    return nullptr;
+}
--- src/server/game/Battlefield/Battlefield.h
+++ src/server/game/Battlefield/Battlefield.h
@@ -15,9 +15,100 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef BATTLEFIELD_H_
-#define BATTLEFIELD_H_
+#ifndef TRINITY_BATTLEFIELD_H_
+#define TRINITY_BATTLEFIELD_H_
 
+#include "Common.h"
+#include "SharedDefines.h"
+#include "Timer.h"
+#include "ZoneScript.h"
+#include <memory>
+#include <unordered_map>
 
+namespace WorldPackets
+{
+    namespace WorldState
+    {
+        class InitWorldStates;
+    }
+}
+
+class BattlefieldGraveyard;
+class ObjectGuid;
+class Player;
+class Unit;
+struct WorldSafeLocsEntry;
+
+class TC_GAME_API Battlefield : public ZoneScript
+{
+public:
+    typedef std::unique_ptr<BattlefieldGraveyard> BattlefieldGraveyardPointer;
+    typedef std::unordered_map<uint8 /*id*/, BattlefieldGraveyardPointer> BattlefieldGraveyardContainer;
+
+    explicit Battlefield(BattlefieldBattleId battleId, BattlefieldZoneId zoneId);
+    virtual ~Battlefield();
+
+    // Called on battlefield creation
+    virtual bool Initialize(bool enabled);
+    virtual void Update(uint32 diff);
+
+    // Called when a player enters the battlefield zone
+    virtual void HandlePlayerEnterZone(Player* player);
+    // Called when a player leaves the battlefield zone
+    virtual void HandlePlayerLeaveZone(Player* player);
+    // Called when a player inside the battlefield zone kills a unit
+    virtual void HandleKill(Player* /*killer*/, Unit* /*victim*/) { }
+    // Called when a player queries a gossip from a spirit healer
+    virtual void HandleAreaSpiritHealerQueryOpcode(Player* player, ObjectGuid source);
+    // Called when a player moves into a resurrection queue
+    virtual void HandleAddPlayerToResurrectionQueue(Player* player, ObjectGuid source);
+    // Called when a player moves out of a resurrection queue
+    virtual void HandleRemovePlayerFromResurrectionQueue(Player* player);
+
+    virtual void FillInitialWorldStates(WorldPackets::WorldState::InitWorldStates& /*packet*/) { }
+    virtual void SendGlobalWorldStates(Player const* /*player*/) const { }
+
+    // Can players inside the battlefield zone use ground mounts?
+    virtual bool IsMountAllowed() const { return true; }
+    // Can players inside the battlefield zone use flying mounts?
+    virtual bool IsFlyingMountAllowed() const { return true; }
+    // Is the referenced SpellArea spellId allowed for the referenced player and newArea?
+    virtual bool IsSpellAreaAllowed(uint32 /*spellId*/, Player const* /*player*/, uint32 /*newArea*/) const { return false; }
+
+    void EmplaceGraveyard(uint8 id, BattlefieldGraveyardPointer&& pointer);
+    BattlefieldGraveyardPointer& GetGraveyard(uint8 graveyardId);
+
+    BattlefieldBattleId GetId() const { return _battleId; }
+    uint32 GetZoneId() const { return _zoneId; }
+    // enum PvPTeamId
+    PvPTeamId GetControllingTeam() const { return _controllingTeam; }
+    // enum PvPTeamId
+    PvPTeamId GetAttackingTeam() const;
+    // enum TeamId
+    TeamId GetControllingTeamId() const;
+    // enum TeamId
+    TeamId GetAttackingTeamId() const;
+    // Battle timer
+    uint32 GetTimer() const { return _timer.GetTimer(); }
+    // Closest available graveyard WorldSafeLocsEntry for Player's TeamId
+    WorldSafeLocsEntry const* GetClosestGraveyardLocation(Player* who) const;
+
+    bool IsEnabled() const { return _enabled; }
+    bool IsWarTime() const { return _active; }
+
+private:
+    Battlefield(Battlefield const&) = delete;
+    Battlefield& operator=(Battlefield const&) = delete;
+
+    // constant information
+    BattlefieldBattleId _battleId;
+    uint32 _zoneId;
+
+    bool _enabled;
+    bool _active;
+    PvPTeamId _controllingTeam;
+    CountdownTimer _timer;
+    BattlefieldGraveyardContainer _graveyards;
+};
 
 #endif
