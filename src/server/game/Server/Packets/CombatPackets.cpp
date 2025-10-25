69237a93c60163de6dbd4279b6fda64a197200e0
ccrs <ccrs@users.noreply.github.com>
2025-10-25 16:05:18 +0200

    Core/Server: include NowDead parameter also on JUST_DEAD units - SAttackStop (experiment)

--- src/server/game/Server/Packets/CombatPackets.cpp
+++ src/server/game/Server/Packets/CombatPackets.cpp
@@ -37,7 +37,7 @@ WorldPackets::Combat::SAttackStop::SAttackStop(Unit const* attacker, Unit const*
     if (victim)
     {
         Victim = victim->GetPackGUID();
-        NowDead = victim->isDead();
+        NowDead = victim->isDead() || victim->isDying();
     }
 }
 
