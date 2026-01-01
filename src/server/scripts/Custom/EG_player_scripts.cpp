48bd7e2195b92b8bb46c5ae99c6035b2b8ffdb3b
ccrs <ccrs@users.noreply.github.com>
2026-01-02 00:22:05 +0100

    clean

--- src/server/scripts/Custom/EG_player_scripts.cpp
+++ src/server/scripts/Custom/EG_player_scripts.cpp
@@ -71,8 +71,7 @@ class EG_WorldChat : public PlayerScript
 
 void AddSC_EG_player_scripts()
 {
-    if (sWorld->getBoolConfig(CONFIG_ACCOUNT_MOUNTS))
-        new EG_AccountMounts();
+    new EG_AccountMounts();
     if (sWorld->getBoolConfig(CONFIG_WORLD_CHAT))
         new EG_WorldChat();
 }
