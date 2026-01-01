35a753b91d20ee18894a400cf63454e025f63d89
ccrs <ccrs@users.noreply.github.com>
2026-01-02 00:25:24 +0100

    final touch

--- src/server/game/Chat/Channels/Channel.cpp
+++ src/server/game/Chat/Channels/Channel.cpp
@@ -558,10 +558,13 @@ void Channel::SetOwner(Player const* player, std::string const& newname)
     Player* newp = ObjectAccessor::FindConnectedPlayerByName(newname);
     ObjectGuid victim = newp ? newp->GetGUID() : ObjectGuid::Empty;
 
-    if (!newp || !victim || !IsOn(victim) ||
-        (player->GetTeam() != newp->GetTeam() &&
-        (!player->GetSession()->HasPermission(rbac::RBAC_PERM_TWO_SIDE_INTERACTION_CHANNEL) ||
-        !newp->GetSession()->HasPermission(rbac::RBAC_PERM_TWO_SIDE_INTERACTION_CHANNEL))))
+    if (!newp
+        || !victim
+        || !IsOn(victim)
+        || (GetName() != WORLD_CHAT
+            && player->GetTeam() != newp->GetTeam()
+            && (!player->GetSession()->HasPermission(rbac::RBAC_PERM_TWO_SIDE_INTERACTION_CHANNEL) || !newp->GetSession()->HasPermission(rbac::RBAC_PERM_TWO_SIDE_INTERACTION_CHANNEL)))
+    )
     {
         PlayerNotFoundAppend appender(newname);
         ChannelNameBuilder<PlayerNotFoundAppend> builder(this, appender);
