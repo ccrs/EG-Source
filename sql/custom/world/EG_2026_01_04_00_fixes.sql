21133abdf7d2ce92a6bf3d8988958bfd05d8a55c
ccrs <ccrs@users.noreply.github.com>
2026-01-04 15:18:51 +0100

    DB: small fixes to quests Samophlange and Deactivating the Spire

---
+++ sql/custom/world/EG_2026_01_04_00_fixes.sql
@@ -0,0 +1,6 @@
+--
+UPDATE `gameobject_template_addon` SET `flags` = 0 WHERE `entry` = 180920;
+UPDATE `gameobject_template_addon` SET `flags` = 0 WHERE `entry` = 180916;
+UPDATE `gameobject_template_addon` SET `flags` = 0 WHERE `entry` = 180919;
+
+UPDATE `gameobject_template_addon` SET `flags` = 0 WHERE `entry` = 61936;
