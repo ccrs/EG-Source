ddf5ee8bd7d08acfa6becf616b54189d9ff2c1ba
ccrs <ccrs@users.noreply.github.com>
2026-01-28 15:34:38 +0100

    DB: include missing Samophlange valves flag correction

--- sql/custom/world/EG_2026_01_04_00_fixes.sql
+++ sql/custom/world/EG_2026_01_04_00_fixes.sql
@@ -3,4 +3,7 @@ UPDATE `gameobject_template_addon` SET `flags` = 0 WHERE `entry` = 180920;
 UPDATE `gameobject_template_addon` SET `flags` = 0 WHERE `entry` = 180916;
 UPDATE `gameobject_template_addon` SET `flags` = 0 WHERE `entry` = 180919;
 
+-- Samophlange valves
+UPDATE `gameobject_template_addon` SET `flags` = 0 WHERE `entry` = 4072;
+UPDATE `gameobject_template_addon` SET `flags` = 0 WHERE `entry` = 61935;
 UPDATE `gameobject_template_addon` SET `flags` = 0 WHERE `entry` = 61936;
