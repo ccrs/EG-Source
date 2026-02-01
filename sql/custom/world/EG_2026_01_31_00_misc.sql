410034e1f51a37581d6fca619e1db3fcdcec2327
ccrs <ccrs@users.noreply.github.com>
2026-02-01 21:41:10 +0100

    DB: hack our way out of poor movement handling

--- sql/custom/world/EG_2026_01_31_00_misc.sql
+++ sql/custom/world/EG_2026_01_31_00_misc.sql
@@ -123,3 +123,5 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 
 
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 22 AND `SourceEntry` = 18944 AND `SourceId` = 0;
+
+UPDATE `waypoint_data` SET `position_x` = -250.17, `position_y` = 1849.67, `position_z` = 94.48, `delay` = 2000 WHERE (`id` = 546513) and (`point` = 12);
