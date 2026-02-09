43c5ca0deb5a09a3a704db0b9a2656a1f0815ce7
ccrs <ccrs@users.noreply.github.com>
2026-02-09 19:06:09 +0100

    DB: cleanup command old value "settings accountMount"

--- sql/custom/world/EG_2026_01_26_00_account_mount.sql
+++ sql/custom/world/EG_2026_01_26_00_account_mount.sql
@@ -1,5 +1,5 @@
 --
-DELETE FROM `command` WHERE `name` IN ('settings account', 'settings account mount', 'settings account riding');
+DELETE FROM `command` WHERE `name` IN ('settings accountMount', 'settings account', 'settings account mount', 'settings account riding');
 INSERT INTO `command` (`name`, `help`) VALUES
 ('settings account', 'Holds different account related settings.'),
 ('settings account mount', 'Syntax: .settings account mount [0/1] - Enables/Disables account mount transfering on character login.'),
