f8444d175dc6958a7190ba43c50b82aadae8c98d
ccrs <ccrs@users.noreply.github.com>
2026-02-10 19:16:38 +0100

    saferish

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -12725,6 +12725,9 @@ void Unit::_ExitVehicle(Position const* exitPosition)
     SetControlled(false, UNIT_STATE_ROOT);      // SMSG_MOVE_FORCE_UNROOT, ~MOVEMENTFLAG_ROOT
 
     AddUnitState(UNIT_STATE_MOVE);
+    bool wasEvading = HasUnitState(UNIT_STATE_EVADE);
+    if (wasEvading)
+        ClearUnitState(UNIT_STATE_EVADE);
 
     if (player)
         player->SetFallInformation(0, GetPositionZ());
@@ -12788,10 +12791,9 @@ void Unit::_ExitVehicle(Position const* exitPosition)
         else
             ToTempSummon()->UnSummon(2000); // Approximation
     }
-    else if (HasUnitState(UNIT_STATE_EVADE) && GetTypeId() == TYPEID_UNIT)
+    else if (wasEvading && GetTypeId() == TYPEID_UNIT)
     {
         Creature* toCreature = ToCreature();
-        toCreature->ClearUnitState(UNIT_STATE_EVADE);
         toCreature->SetSpawnHealth();
         toCreature->LoadCreaturesAddon();
         if (toCreature->IsVehicle())
