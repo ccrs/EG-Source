2666e1ad65ff540e0e2eb02516b5229ebd0add11
ccrs <ccrs@users.noreply.github.com>
2026-02-11 15:15:53 +0100

    Core/Custom: implement new unit base searcher - Find near Unit with the lowest HP

--- src/server/game/Custom/CustomFunctions.cpp
+++ src/server/game/Custom/CustomFunctions.cpp
@@ -3,6 +3,7 @@
 #include "Log.h"
 #include "Player.h"
 #include "Transmogrification.h"
+#include "Unit.h"
 #include "CustomFunctions.h"
 
 void Player::_LoadCustomSettings(PreparedQueryResult result)
@@ -171,4 +172,21 @@ Loot* Player::GetLootFromAOELoot(ObjectGuid lootGUID) const
             return currentLoot.RelatedLoot;
     }
     return nullptr;
+}
+
+bool EG::MostHPMissingFriendlyUnitInRangeSearcher::operator()(Unit *unit)
+{
+    if ((_playerOnly && unit->IsPlayer() || !_playerOnly)
+        && unit->IsAlive()
+        && unit->IsInCombat()
+        && _source->IsValidAssistTarget(unit)
+        && _source->IsWithinDistInMap(unit, _range)
+        && (unit->GetMaxHealth() - unit->GetHealth() > _hp)
+    )
+    {
+        _hp = unit->GetMaxHealth() - unit->GetHealth();
+        return true;
+    }
+
+    return false;
 }
