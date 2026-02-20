c08b1b47745d57e9d4b5b1e1e40d105a30d1fd77
ccrs <ccrs@users.noreply.github.com>
2026-02-20 20:59:05 +0100

    DB: make the wall NPCs used as hook points actually appear on the wall - The Drakkensryd

--- sql/custom/world/EG_2026_02_20_00_misc.sql
+++ sql/custom/world/EG_2026_02_20_00_misc.sql
@@ -10,3 +10,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 
 
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 22 AND `SourceEntry` = 30575 AND `SourceId` = 0;
+
+DELETE FROM `creature_template_movement` WHERE `CreatureId`= 29754;
+INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`, `InteractionPauseTimer`) VALUES
+(29754, 0, 0, 1, 1, 0, 0, 0);
