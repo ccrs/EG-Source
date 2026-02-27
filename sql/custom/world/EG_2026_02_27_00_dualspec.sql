e93246f7261ed145e43b0d2c2640323e3e1bb362
ccrs <ccrs@users.noreply.github.com>
2026-02-27 13:19:58 +0100

    Custom: reduce dual spec required money to 100g

---
+++ sql/custom/world/EG_2026_02_27_00_dualspec.sql
@@ -0,0 +1,4 @@
+-- 
+UPDATE `gossip_menu_option` SET `BoxMoney` = 1000000 WHERE (`MenuID` = 0) and (`OptionID` = 16);
+UPDATE `gossip_menu_option` SET `BoxMoney` = 1000000 WHERE (`MenuID` = 6647) and (`OptionID` = 2);
+UPDATE `gossip_menu_option` SET `BoxMoney` = 1000000 WHERE (`MenuID` = 10371) and (`OptionID` = 0);
