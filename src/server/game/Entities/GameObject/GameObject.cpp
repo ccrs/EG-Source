75f54d865f2aa4ebbd9a09bbbf91db08972844d5
ccrs <ccrs@users.noreply.github.com>
2026-03-04 00:47:21 +0100

    Core/Entities: allow gameobject usage with partyOnly flag (e.g. mage portals) if the owner is not connected but still part of the group

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -2034,11 +2034,16 @@ void GameObject::Use(Unit* user)
 
             if (info->spellcaster.partyOnly)
             {
-                Unit* caster = GetOwner();
-                if (!caster || caster->GetTypeId() != TYPEID_PLAYER)
+                ObjectGuid ownerGuid = GetOwnerGUID();
+                if (!ownerGuid || !ownerGuid.IsPlayer() || user->GetTypeId() != TYPEID_PLAYER)
                     return;
 
-                if (user->GetTypeId() != TYPEID_PLAYER || !user->ToPlayer()->IsInSameRaidWith(caster->ToPlayer()))
+                if (Group* group = user->ToPlayer()->GetGroup())
+                {
+                    if (!group->IsMember(ownerGuid))
+                        return;
+                }
+                else if (ownerGuid != user->GetGUID())
                     return;
             }
 
