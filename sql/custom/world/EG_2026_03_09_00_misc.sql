e3b55cf2b34bd6245e4f209268675dbd83b1832a
ccrs <ccrs@users.noreply.github.com>
2026-03-09 19:34:48 +0100

    DB: remove SPELLCLICK flag from Steel Gate Flying Machine, lets force everyone to use the npc...

--- sql/custom/world/EG_2026_03_09_00_misc.sql
+++ sql/custom/world/EG_2026_03_09_00_misc.sql
@@ -3,3 +3,6 @@ UPDATE `creature_model_info` SET `BoundingRadius` = 0.50915, `CombatReach` = 1.0
 UPDATE `creature_template` SET `AIName` = '', `ScriptName` = 'EG_npc_plague_slime' WHERE `entry` = 16243;
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 16243 AND `source_type` = 0;
 UPDATE `creature_template` SET `ScriptName` = 'EG_npc_evolving_ectoplasm' WHERE `entry` = 3640;
+
+-- Steel Gate Flying Machine
+UPDATE `creature_template` SET `npcflag` = 0 WHERE `entry` = 24418;
