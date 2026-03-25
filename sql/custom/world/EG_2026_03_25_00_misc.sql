a768f0ee86116b266a6b62f5f29a2c30e6e8a3e0
ccrs <ccrs@users.noreply.github.com>
2026-03-26 00:25:13 +0100

    DB: rework Conqueror Krenna from quest The Conquest Pit: Final Showdown

--- sql/custom/world/EG_2026_03_25_00_misc.sql
+++ sql/custom/world/EG_2026_03_25_00_misc.sql
@@ -70,3 +70,39 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 
 
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 22 AND `SourceEntry` = 27718 AND `SourceId` = 0;
+
+ -- Conqueror Krenna smart ai
+SET @ENTRY := 27727;
+UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE `entry` = @ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryOrGuid` = @ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(@ENTRY, 0, 0, 0, 9, 0, 100, 0, 0, 5, 7000, 9000, 11, 15284, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'When victim in range 0 - 5 yards (cooldown 7000 - 9000 ms) - Self: Cast spell  Cleave (15284) on Victim'),
+(@ENTRY, 0, 1, 0, 0, 0, 100, 0, 9000, 12000, 12000, 17000, 11, 11430, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Every 12 - 17 seconds (9 - 12s initially) (IC) - Self: Cast spell  Slam (11430) on Victim'),
+(@ENTRY, 0, 2, 0, 13, 0, 100, 0, 14000, 17000, 0, 0, 11, 12555, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'When victim is casting anyspell (once) - Self: Cast spell  Pummel (12555) on Victim'),
+(@ENTRY, 0, 3, 0, 0, 0, 100, 0, 3000, 6000, 6000, 11000, 11, 34719, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Every 6 - 11 seconds (3 - 6s initially) (IC) - Self: Cast spell  Fixate (34719) on Victim'),
+(@ENTRY, 0, 4, 5, 54, 0, 100, 0, 0, 0, 0, 0, 12, 27726, 3, 75000, 0, 0, 0, 8, 0, 0, 0, 3244.5, -2340.56, 92.14, 6.27, 'On just summoned - Self: Summon creature Gorgonna (27726) at (3244.5, -2340.56, 92.14, 6.27) as summon type timed despawn with duration 75 seconds'),
+(@ENTRY, 0, 5, 6, 61, 0, 100, 0, 0, 0, 0, 0, 1, 0, 18000, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'On just summoned - Self: Talk 0 to invoker'),
+(@ENTRY, 0, 6, 0, 61, 0, 100, 0, 0, 0, 0, 0, 2, 14, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'On just summoned - Self: Set faction to Monster (14)'),
+(@ENTRY, 0, 7, 0, 52, 0, 100, 0, 0, 27727, 0, 0, 1, 1, 6000, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'When text 0 said by creature Conqueror Krenna (27727) is over - Self: Talk 1 to invoker'),
+(@ENTRY, 0, 8, 0, 6, 0, 100, 0, 0, 0, 0, 0, 26, 12431, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'On death - Killer: Call group event happened from quest The Conquest Pit: Final Showdown (12431) on owner'),
+(@ENTRY, 0, 9, 0, 7, 0, 100, 0, 0, 0, 0, 0, 6, 12431, 0, 0, 0, 0, 0, 18, 25, 0, 0, 0, 0, 0, 0, 'On evade - Players in 25 yards: Fail quest The Conquest Pit: Final Showdown (12431)'),
+(@ENTRY, 0, 10, 0, 21, 0, 100, 0, 0, 0, 0, 0, 2, 14, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'On home reached - Self: Set faction to Monster (14)');
+
+
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 22 AND `SourceEntry` = 27727 AND `SourceId` = 0;
+
+ -- Gorgonna smart ai
+SET @ENTRY := 27726;
+UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE `entry` = @ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryOrGuid` = @ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(@ENTRY, 0, 0, 0, 1, 0, 100, 1, 6000, 6000, 0, 0, 1, 0, 6000, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Time = 6 seconds (OOC) - Self: Talk 0 to invoker'),
+(@ENTRY, 0, 1, 0, 52, 0, 100, 0, 0, 27726, 0, 0, 1, 1, 18000, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'When text 0 said by creature Gorgonna (27726) is over - Self: Talk 1 to invoker'),
+(@ENTRY, 0, 2, 0, 52, 0, 100, 0, 1, 27726, 0, 0, 1, 2, 15000, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'When text 1 said by creature Gorgonna (27726) is over - Self: Talk 2 to invoker'),
+(@ENTRY, 0, 3, 0, 52, 0, 100, 0, 2, 27726, 0, 0, 1, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'When text 2 said by creature Gorgonna (27726) is over - Self: Talk 3 to invoker'),
+(@ENTRY, 0, 4, 0, 75, 0, 100, 0, 0, 27727, 50, 2000, 49, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'When creature Conqueror Krenna (27727) in range 50 (cooldown 2 sec) - Self: Attack Found creature');
+
+
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 22 AND `SourceEntry` = 27726 AND `SourceId` = 0;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `Comment`) VALUES 
+(22, 5, 27726, 0, 0, 21, 1, 32, 0, 0, 0, 'Object has unit state UNIT_STATE_CHASE');
