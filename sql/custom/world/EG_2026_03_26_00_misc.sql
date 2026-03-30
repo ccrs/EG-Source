f818f97199e5c79fcd7da92394616c82eb2ca85e
ccrs <ccrs@users.noreply.github.com>
2026-03-30 06:05:35 +0200

    DB: cleanup useless start up error...

--- sql/custom/world/EG_2026_03_26_00_misc.sql
+++ sql/custom/world/EG_2026_03_26_00_misc.sql
@@ -6,7 +6,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (@ENTRY, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'On aggro - Self: Talk 0 to Attacked unit'),
 (@ENTRY, 0, 1, 0, 0, 0, 100, 0, 0, 0, 3500, 4100, 11, 41440, 64, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Every 3.5 - 4.1 seconds (0 - 0s initially) (IC) - Self: Cast spell  Shoot (41440) with flags combat move on Victim'),
 (@ENTRY, 0, 2, 0, 38, 0, 100, 0, 1, 1, 0, 0, 22, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'On data[1] set to 1 - Set event phase to phase 1'),
-(@ENTRY, 0, 3, 0, 1, 1, 100, 0, 60000, 60000, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Time = 60 seconds (OOC) - Self: Despawn instantly');
+(@ENTRY, 0, 3, 0, 1, 1, 100, 1, 60000, 60000, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Time = 60 seconds (OOC) - Self: Despawn instantly');
 
 
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 22 AND `SourceEntry` = 25801 AND `SourceId` = 0;
