ca898823ef363dcea50505c8a4e9ceef39370063
ccrs <ccrs@users.noreply.github.com>
2026-04-03 14:48:23 +0200

    Core/Spells: "improve" pet taming handling - hacky stuff
    (cherry picked from commit 4b56e7fbee9413d43e0537ececb4431dc3786c70)

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -11645,6 +11645,7 @@ bool Unit::SetCharmedBy(Unit* charmer, CharmType type, AuraApplication const* au
 
     // Set charmed
     charmer->SetCharm(this, true);
+    m_combatManager.RevalidateCombat();
 
     if (Player* player = ToPlayer())
     {
