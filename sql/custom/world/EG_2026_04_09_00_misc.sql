6b9dc19985f1289e9aee70446576641517642699
ccrs <ccrs@users.noreply.github.com>
2026-04-09 12:34:03 +0200

    DB: rescript Jotunheim Proto-Drake

--- sql/custom/world/EG_2026_04_09_00_misc.sql
+++ sql/custom/world/EG_2026_04_09_00_misc.sql
@@ -1,2 +1,12 @@
 --
 UPDATE `item_template` SET `duration` = 86400 WHERE `entry` = 39878;
+
+ -- Jotunheim Proto-Drake smart ai
+SET @ENTRY := 30330;
+UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE `entry` = @ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryOrGuid` = @ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(@ENTRY, 0, 0, 0, 28, 0, 100, 0, 0, 0, 0, 0, 146, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'On passenger removed (vehicle) - Passenger: Set interactable');
+
+
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 22 AND `SourceEntry` = 30330 AND `SourceId` = 0;
