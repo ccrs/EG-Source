fc79a68d923924b81e7531b8dd06e49d74990869
ccrs <ccrs@users.noreply.github.com>
2026-04-18 04:41:36 +0200

    Build: fix warning
    /home/runner/work/EG-Source/EG-Source/src/server/game/Spells/Auras/SpellAuraEffects.cpp: In member function ‘void AuraEffect::HandleAuraTransform(const AuraApplication*, uint8, bool) const’:
    /home/runner/work/EG-Source/EG-Source/src/server/game/Spells/Auras/SpellAuraEffects.cpp:2104:28: error: suggest explicit braces to avoid ambiguous ‘else’ [-Werror=dangling-else]
     2104 |                         if (Unit* caster = GetCaster())
          |                            ^
    compilation terminated due to -Wfatal-errors.
    cc1plus: all warnings being treated as errors
    make[2]: *** [src/server/game/CMakeFiles/game.dir/build.make:3663: src/server/game/CMakeFiles/game.dir/Spells/Auras/SpellAuraEffects.cpp.o] Error 1

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -2102,10 +2102,12 @@ void AuraEffect::HandleAuraTransform(AuraApplication const* aurApp, uint8 mode, 
                     // Polymorph (sheep)
                     if (GetSpellInfo()->SpellFamilyName == SPELLFAMILY_MAGE && GetSpellInfo()->SpellIconID == 82 && GetSpellInfo()->SpellVisual[0] == 12978)
                         if (Unit* caster = GetCaster())
+                        {
                             if (caster->HasAura(52648))         // Glyph of the Penguin
                                 model_id = 26452;
                             else if (caster->IsPlayer() && caster->ToPlayer()->HasCustomFlag(CustomFlagsIndex::CUSTOM_VISUALS, CustomFlags::CUSTOM_FLAG_VISUALS_MAGE_ACTIVE))
                                 model_id = 1060;
+                        }
 
                     target->SetDisplayId(model_id);
 
