c8f4cafb646cd8239bd55530d02693016588b162
ccrs <ccrs@users.noreply.github.com>
2026-04-21 17:30:01 +0200

    DB: nerf Twisted Visage damage...

--- sql/custom/world/EG_2026_04_20_00_misc.sql
+++ sql/custom/world/EG_2026_04_20_00_misc.sql
@@ -1,2 +1,5 @@
 --
 UPDATE `creature_template_addon` SET `auras` = '19483' WHERE `entry` = 89;
+
+UPDATE `creature_template` SET `BaseAttackTime` = 2400, `DamageModifier` = 7 WHERE `entry` = 31480;
+UPDATE `creature_template` SET `DamageModifier` = 5 WHERE `entry` = 30625;
