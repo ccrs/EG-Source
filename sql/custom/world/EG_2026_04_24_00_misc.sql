26ca0a7892479be48b02620fbd677b649ea84e94
ccrs <ccrs@users.noreply.github.com>
2026-04-24 16:54:35 +0200

    Misc: go down the rabbit hole and enable more pet scaling auras...
    (cherry picked from commit 2329a545f24f0faa8820a23d4fe7002a0fa7b93c)

--- sql/custom/world/EG_2026_04_24_00_misc.sql
+++ sql/custom/world/EG_2026_04_24_00_misc.sql
@@ -3,8 +3,24 @@ DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_dk_avoidance_passiv
 INSERT INTO `spell_script_names` (spell_id, ScriptName) VALUES
 (62137, 'spell_dk_avoidance_passive');
 
--- DK ghoul auras
+-- Pet hit/crit/expertise inheritance (spell_gen_pet_calculate)
+-- 19591 = hunter tamed pet passive 06 (melee + spell crit from owner)
+-- 35695 = warlock pet passive crit (spell + melee crit from owner)
+-- 61013 = warlock pet scaling 05 (melee hit + spell hit + expertise from owner)
+-- 61017 = hunter pet scaling 04 (melee hit + spell hit + expertise from owner)
+-- 61697 = DK pet scaling 03 (melee hit + spell hit from owner)
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (19591, 35695, 61013, 61017, 61697) AND `ScriptName` = 'spell_gen_pet_calculate';
+INSERT INTO `spell_script_names` (spell_id, ScriptName) VALUES
+(19591, 'spell_gen_pet_calculate'),
+(35695, 'spell_gen_pet_calculate'),
+(61013, 'spell_gen_pet_calculate'),
+(61017, 'spell_gen_pet_calculate'),
+(61697, 'spell_gen_pet_calculate');
+
+-- DK ghoul auras (no creature family, not covered by LearnPetPassives)
+-- 62137 = DK avoidance passive (Night of the Dead talent scaling)
+-- 61697 = DK pet scaling 03 (hit/spell hit from owner)
 DELETE FROM `creature_template_addon` WHERE entry IN (26125, 24207);
 INSERT INTO `creature_template_addon` (entry, auras) VALUES
-(26125, '62137'),
-(24207, '62137');
+(26125, '62137 61697'),
+(24207, '62137 61697');
