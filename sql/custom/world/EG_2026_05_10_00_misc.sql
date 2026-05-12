b52016d2447a0ae7636e893c623bb6aa3bb51340
ccrs <ccrs@users.noreply.github.com>
2026-05-12 11:33:57 +0200

    DB: fix startup error, finally

--- sql/custom/world/EG_2026_05_10_00_misc.sql
+++ sql/custom/world/EG_2026_05_10_00_misc.sql
@@ -1,2 +1,3 @@
 --
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 13 AND `SourceEntry` IN (55853, 56263) AND `SourceGroup` = 1;
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 13 AND `SourceEntry` IN (55853, 56263) AND `SourceGroup` = 0;
