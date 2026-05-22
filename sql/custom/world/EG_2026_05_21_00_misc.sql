ba368851ca0a2ebf95b996902d163b758dd6cc82
ccrs <ccrs@users.noreply.github.com>
2026-05-22 15:30:46 +0200

    DB: shut up more db warnings

--- sql/custom/world/EG_2026_05_21_00_misc.sql
+++ sql/custom/world/EG_2026_05_21_00_misc.sql
@@ -12,3 +12,10 @@ INSERT INTO `creature_text` (`CreatureID`,`GroupID`,`ID`,`Text`,`Type`,`Language
 (12938,0,0,"I'm saved! Thank you, doctor!",12,0,100,0,0,0,8355,0,"");
 
 UPDATE `gameobject` SET `spawntimesecs` = -1 WHERE `guid` IN (11757, 60069, 60080, 60095);
+
+DELETE FROM `spawn_group` WHERE `spawnType` = 1 AND `spawnId` IN (11757, 60069, 60080, 60095);
+INSERT INTO `spawn_group` (`groupId`, `spawnType`, `spawnId`) VALUES
+(1, 1, 11757),
+(1, 1, 60069),
+(1, 1, 60080),
+(1, 1, 60095);
