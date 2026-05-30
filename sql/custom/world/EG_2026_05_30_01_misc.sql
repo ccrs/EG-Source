9375c08e6f80e09e466c2554d0de39d385f3dd79
ccrs <ccrs@users.noreply.github.com>
2026-05-31 00:29:00 +0200

    DB: add internal cooldown to Twilight Torment proc

--- sql/custom/world/EG_2026_05_30_01_misc.sql
+++ sql/custom/world/EG_2026_05_30_01_misc.sql
@@ -6,3 +6,8 @@ DELETE FROM `creature_template_movement` WHERE `CreatureId` IN (30234,31748);
 INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`) VALUES
 (30234, 1, 0, 1, 0, 0, 0),
 (31748, 1, 0, 1, 0, 0, 0);
+
+DELETE FROM `spell_proc` WHERE `SpellId` IN (57935, 58835);
+INSERT INTO `spell_proc` (`SpellId`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `DisableEffectsMask`, `ProcsPerMinute`, `Chance`, `Cooldown`, `Charges`) VALUES
+(57935, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 1500, 0),
+(58835, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 1500, 0);
