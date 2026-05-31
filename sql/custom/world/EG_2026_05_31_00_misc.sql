07b92a16c7a857678a4ad81ee0985ee432ea061a
ccrs <ccrs@users.noreply.github.com>
2026-05-31 16:40:59 +0200

    DB: review Crystallized Life drop rates

---
+++ sql/custom/world/EG_2026_05_31_00_misc.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `gameobject_loot_template` SET `Chance` = 24 WHERE `Entry` IN (24093, 24224, 24225) AND `Item` = 37704;
+UPDATE `gameobject_loot_template` SET `Chance` = 27 WHERE `Entry` IN (24226, 24227, 24228, 25089, 25093) AND `Item` = 37704;
