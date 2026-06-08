540fa6b86cd13aa37138a0e9597ed580dfd29b32
ccrs <ccrs@users.noreply.github.com>
2026-06-08 23:48:48 +0200

    DB: remove incorrect loot entries from XT-002 Deconstructor

--- sql/custom/world/EG_2026_06_08_00_misc.sql
+++ sql/custom/world/EG_2026_06_08_00_misc.sql
@@ -4,3 +4,5 @@ DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 19 AND `SourceEntry` 
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (19, 0, 13108, 0, 0, 8, 0, 13047, 0, 0, 0, 0, 0, '', 'Whatever it Takes! requires only The Reckoning (13047)'),
 (19, 0, 13109, 0, 0, 8, 0, 13047, 0, 0, 0, 0, 0, '', 'Diametrically Opposed requires only The Reckoning (13047)');
+
+DELETE FROM `creature_loot_template` WHERE `Entry`=33293 AND `Item` IN (45442, 45443, 45444, 45445, 45446);
