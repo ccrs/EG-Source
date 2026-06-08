d33900360fb5d078d27f71d71f115a8e0c02ffec
ccrs <ccrs@users.noreply.github.com>
2026-06-08 20:35:13 +0200

    DB/Quest: King Jokkum follow-ups require only The Reckoning

---
+++ sql/custom/world/EG_2026_06_08_00_misc.sql
@@ -0,0 +1,6 @@
+--
+UPDATE `quest_template_addon` SET `PrevQuestID` = 0 WHERE `ID` IN (13108, 13109);
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 19 AND `SourceEntry` IN (13108, 13109);
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(19, 0, 13108, 0, 0, 8, 0, 13047, 0, 0, 0, 0, 0, '', 'Whatever it Takes! requires only The Reckoning (13047)'),
+(19, 0, 13109, 0, 0, 8, 0, 13047, 0, 0, 0, 0, 0, '', 'Diametrically Opposed requires only The Reckoning (13047)');
