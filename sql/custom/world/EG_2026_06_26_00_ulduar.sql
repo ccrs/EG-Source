a90d9544512b01aa37db8f57e38a90281e085d7f
ccrs <ccrs@users.noreply.github.com>
2026-06-26 18:01:58 +0200

    DB: fix Archivum Data Disc drop in Iron Council encounter

---
+++ sql/custom/world/EG_2026_06_26_00_ulduar.sql
@@ -0,0 +1,9 @@
+--
+-- Iron Council: Archivum Data Disc must also drop on hard mode (Steelbreaker last), not only medium (Molgeim last)
+DELETE FROM `creature_loot_template` WHERE `Entry` = 32867 AND `Item` = 45506;
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(32867, 45506, 0, 100, 0, 1, 0, 1, 1, 'Archivum Data Disc - hard mode (Steelbreaker last)');
+
+DELETE FROM `creature_loot_template` WHERE `Entry` = 33693 AND `Item` = 45857;
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(33693, 45857, 0, 100, 0, 1, 0, 1, 1, 'Archivum Data Disc - hard mode (Steelbreaker last)');
