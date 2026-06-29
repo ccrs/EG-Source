37bed6cc6ab12c788afeccdc3225f46a9339aa28
ccrs <ccrs@users.noreply.github.com>
2026-06-29 14:24:31 +0200

    DB: reduce immunities to Charged Spheres

--- sql/custom/world/EG_2026_06_26_00_ulduar.sql
+++ sql/custom/world/EG_2026_06_26_00_ulduar.sql
@@ -7,3 +7,6 @@ INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 DELETE FROM `creature_loot_template` WHERE `Entry` = 33693 AND `Item` = 45857;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (33693, 45857, 0, 100, 0, 1, 0, 1, 1, 'Archivum Data Disc - hard mode (Steelbreaker last)');
+
+UPDATE `creature_template` SET `mechanic_immune_mask` = 537082687 WHERE `entry` = 33715;
+UPDATE `creature_template` SET `mechanic_immune_mask` = 537082687 WHERE `entry` = 33756;
