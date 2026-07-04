6556d10ac24b8e283c40d3017131dc8f57687e89
ccrs <ccrs@users.noreply.github.com>
2026-07-04 15:14:22 +0200

    DB: fix damage output for more entries from classic dungeons

--- sql/custom/world/EG_2026_07_02_00_misc.sql
+++ sql/custom/world/EG_2026_07_02_00_misc.sql
@@ -1,3 +1,28 @@
 --
-UPDATE `creature_template` SET `DamageModifier` = 2 WHERE `entry` = 639;
-UPDATE `creature_template` SET `DamageModifier` = 2 WHERE `entry` = 643;
+UPDATE `creature_template` SET `DamageModifier` = 2 WHERE `entry` = 639; -- Edwin VanCleef
+UPDATE `creature_template` SET `DamageModifier` = 2 WHERE `entry` = 643; -- Sneed
+--
+UPDATE `creature_template` SET `DamageModifier` = 1.7 WHERE `entry` = 5762; -- Deviate Moccasin
+UPDATE `creature_template` SET `DamageModifier` = 1.7 WHERE `entry` = 5775; -- Verdan the Everliving
+UPDATE `creature_template` SET `DamageModifier` = 2.5 WHERE `entry` = 3654; -- Mutanus the Devourer
+--
+UPDATE `creature_template` SET `DamageModifier` = 2.4 WHERE `entry` = 12237; -- Meshlok the Harvester
+--
+UPDATE `creature_template` SET `DamageModifier` = 1.7 WHERE `entry` = 3865; -- Shadow Charger
+--
+UPDATE `creature_template` SET `DamageModifier` = 1.7 WHERE `entry` = 4978; -- Aku'mai Servant
+--
+UPDATE `creature_template` SET `DamageModifier` = 1.7 WHERE `entry` = 7361; -- Grubbis
+UPDATE `creature_template` SET `DamageModifier` = 1.7 WHERE `entry` = 6215; -- Chomper
+--
+UPDATE `creature_template` SET `DamageModifier` = 1.7 WHERE `entry` = 7355; -- Tuten'kash
+UPDATE `creature_template` SET `DamageModifier` = 1.7 WHERE `entry` = 7356; -- Plaguemaw the Rotting
+UPDATE `creature_template` SET `DamageModifier` = 1.7 WHERE `entry` = 7354; -- Ragglesnout
+UPDATE `creature_template` SET `DamageModifier` = 1.7 WHERE `entry` = 7351; -- Tomb Reaver
+--
+UPDATE `creature_template` SET `DamageModifier` = 2.4 WHERE `entry` = 4293; -- Scarlet Scryer
+UPDATE `creature_template` SET `DamageModifier` = 2.4 WHERE `entry` = 6490; -- Azshir the Sleepless
+--
+UPDATE `creature_template` SET `DamageModifier` = 3.5 WHERE `entry` = 1853; -- Darkmaster Gandling
+UPDATE `creature_template` SET `DamageModifier` = 3.5 WHERE `entry` = 10506; -- Kirtonos the Herald
+UPDATE `creature_template` SET `DamageModifier` = 3.5 WHERE `entry` = 16118; -- Kormok
