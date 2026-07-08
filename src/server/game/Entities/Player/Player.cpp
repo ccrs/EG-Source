a4d881ae199b10aad0a099f6017f66c1d707cffc
ccrs <ccrs@users.noreply.github.com>
2026-07-08 13:37:56 +0200

    Merge TrinityCore 3.3.5 48716696d951a3cea5a98e65685f784675c56069

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -325,9 +325,7 @@ Player::Player(WorldSession* session): Unit(true)
     isDebugAreaTriggers = false;
 
     m_WeeklyQuestChanged = false;
-
     m_MonthlyQuestChanged = false;
-
     m_SeasonalQuestChanged = false;
 
     SetPendingBind(0, 0);
@@ -18379,10 +18377,8 @@ void Player::_LoadQuestStatusRewarded(PreparedQueryResult result)
 
                 // set rewarded title if any
                 if (quest->GetRewTitle())
-                {
                     if (CharTitlesEntry const* titleEntry = sCharTitlesStore.LookupEntry(quest->GetRewTitle()))
                         SetTitle(titleEntry);
-                }
 
                 if (uint32 talents = quest->GetBonusTalents())
                     AddQuestRewardedTalentCount(talents);
@@ -22528,9 +22524,6 @@ void Player::SendInitialPacketsBeforeAddToMap()
 
     /// SMSG_SEND_UNLEARN_SPELLS
     SendUnlearnSpells();
-
-    /// @todo: SMSG_SEND_SPELL_HISTORY
-    /// @todo: SMSG_SEND_SPELL_CHARGES
 
     /// SMSG_ACTION_BUTTONS
     SendInitialActionButtons();
