2564989c44afb68db2ffaa40a36798aa9398557f
ccrs <ccrs@users.noreply.github.com>
2026-07-09 07:41:19 +0200

    Core/Handlers: revert d4046b1565d13fa3aeb2a60d250de6daf20caa27
    (cherry picked from commit 74b3829d176fb3a41899636ab2627b7637f09718)

--- src/server/game/Handlers/MovementHandler.cpp
+++ src/server/game/Handlers/MovementHandler.cpp
@@ -246,19 +246,6 @@ void WorldSession::HandleMoveWorldportAck()
     player->ResetMap();
     player->SetMap(newMap);
 
-    // EG - proper resurrection sequence for dead teleporting players
-    // Resurrect before AddPlayerToMap so SendInitSelf sends alive state; dead state would fire "has died" on the client.
-    if (!player->IsAlive())
-    {
-        if (player->GetTeleportOptions() & TELE_REVIVE_AT_TELEPORT)
-            player->ResurrectPlayer(0.5f);
-        else if (mEntry->IsDungeon() && player->GetCorpseLocation().GetMapId() == mEntry->ID)
-        {
-            player->ResurrectPlayer(0.5f);
-            player->SpawnCorpseBones();
-        }
-    }
-
     player->SendInitialPacketsBeforeAddToMap();
     if (!player->GetMap()->AddPlayerToMap(player))
     {
@@ -309,6 +296,19 @@ void WorldSession::HandleMoveWorldportAck()
 
         // stop flight
         player->FinishTaxiFlight();
+    }
+
+    if (!player->IsAlive() && player->GetTeleportOptions() & TELE_REVIVE_AT_TELEPORT)
+        player->ResurrectPlayer(0.5f);
+
+    // resurrect character at enter into instance where his corpse exist after add to map
+    if (mEntry->IsDungeon() && !player->IsAlive())
+    {
+        if (player->GetCorpseLocation().GetMapId() == mEntry->ID)
+        {
+            player->ResurrectPlayer(0.5f);
+            player->SpawnCorpseBones();
+        }
     }
 
     bool mountAllowed = false;
