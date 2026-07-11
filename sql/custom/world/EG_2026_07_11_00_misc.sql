ea265107b7a207baabb066aaeb6af92242eecfbe
ccrs <ccrs@users.noreply.github.com>
2026-07-11 03:33:56 +0200

    DB: fix Drak'Tharon's Cleansing quest event

--- sql/custom/world/EG_2026_07_11_00_misc.sql
+++ sql/custom/world/EG_2026_07_11_00_misc.sql
@@ -4,3 +4,6 @@ DELETE FROM `npc_vendor` WHERE `entry` = 33964 AND  `item` = 42852;
 INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES
 (33963, 0, 42852, 0, 0, 2631, 0),
 (33964, 0, 42852, 0, 0, 2631, 0);
+
+UPDATE `waypoint_data` SET `position_z` = 132.0 WHERE `id` = 227985 AND `point` IN (1, 3);
+UPDATE `smart_scripts` SET `target_z` = 132.0 WHERE `entryorguid` = 2801600 AND `source_type` = 9 AND `id` = 11;
