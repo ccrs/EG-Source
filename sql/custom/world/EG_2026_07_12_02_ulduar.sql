4b6c930717eee15ae0c02a9f8abd9faff75e5393
ccrs <ccrs@users.noreply.github.com>
2026-07-12 20:56:54 +0200

    Misc/Ulduar: fix loot distribution in Leviathan...

---
+++ sql/custom/world/EG_2026_07_12_02_ulduar.sql
@@ -0,0 +1,11 @@
+--
+UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 2 WHERE `Entry` = 33113 AND `Item` = 3;
+UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 2 WHERE `Entry` = 33113 AND `Item` = 5;
+UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 3 WHERE `Entry` = 33113 AND `Item` = 7;
+UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 3 WHERE `Entry` = 33113 AND `Item` = 9;
+
+UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 1 WHERE `Entry` = 34003 AND `Item` = 1;
+UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 2 WHERE `Entry` = 34003 AND `Item` = 3;
+UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 2 WHERE `Entry` = 34003 AND `Item` = 5;
+UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 3 WHERE `Entry` = 34003 AND `Item` = 7;
+UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 3 WHERE `Entry` = 34003 AND `Item` = 9;
