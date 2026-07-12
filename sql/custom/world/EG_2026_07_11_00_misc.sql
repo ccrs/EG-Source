67739b9372c50f3ce4a119bc0ba3ea82302e3c26
ccrs <ccrs@users.noreply.github.com>
2026-07-12 15:18:53 +0200

    DB: make Trollgore stay fucking put

--- sql/custom/world/EG_2026_07_11_00_misc.sql
+++ sql/custom/world/EG_2026_07_11_00_misc.sql
@@ -7,3 +7,6 @@ INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `Exte
 
 UPDATE `waypoint_data` SET `position_z` = 132.0 WHERE `id` = 227985 AND `point` IN (1, 3);
 UPDATE `smart_scripts` SET `target_z` = 132.0 WHERE `entryorguid` = 2801600 AND `source_type` = 9 AND `id` = 11;
+
+--
+UPDATE `creature` SET `MovementType`=0, `wander_distance`=0 WHERE `guid`=127422;
