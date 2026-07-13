ba6f58625bf0bf0f62a4a09ed57f9aa83e82659d
ccrs <ccrs@users.noreply.github.com>
2026-07-13 02:51:14 +0200

    DB: fix emblem distribution counts for Flame Leviathan

--- sql/custom/world/EG_2026_07_12_02_ulduar.sql
+++ sql/custom/world/EG_2026_07_12_02_ulduar.sql
@@ -1,11 +1,3 @@
 --
-UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 2 WHERE `Entry` = 33113 AND `Item` = 3;
-UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 2 WHERE `Entry` = 33113 AND `Item` = 5;
-UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 3 WHERE `Entry` = 33113 AND `Item` = 7;
-UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 3 WHERE `Entry` = 33113 AND `Item` = 9;
-
-UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 1 WHERE `Entry` = 34003 AND `Item` = 1;
-UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 2 WHERE `Entry` = 34003 AND `Item` = 3;
-UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 2 WHERE `Entry` = 34003 AND `Item` = 5;
-UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 3 WHERE `Entry` = 34003 AND `Item` = 7;
-UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 3 WHERE `Entry` = 34003 AND `Item` = 9;
+UPDATE `creature_loot_template` SET `MinCount` = 1, `MaxCount` = 1 WHERE `Entry` IN (33113, 34003) AND `Item` IN (1, 3, 5, 7);
+DELETE FROM `creature_loot_template` WHERE `Entry` IN (33113, 34003) AND `Item` = 9;
