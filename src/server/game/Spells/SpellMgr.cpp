dd436fab9198ded43c8914cd801da720705824e8
ccrs <ccrs@users.noreply.github.com>
2026-09-04 12:15:19 +0200

    Core/Spells: change Twisting Blade to UNIT_TARGET_ENEMY to prevent self harm to the vehicle caster

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -5265,6 +5265,12 @@ void SpellMgr::LoadSpellInfoCorrections()
         spellInfo->_GetEffect(EFFECT_0).ApplyAuraPeriod = 15 * IN_MILLISECONDS;;
     });
 
+    // Twisting Blade
+    ApplySpellFix({ 47938 }, [](SpellInfo* spellInfo)
+    {
+        spellInfo->_GetEffect(EFFECT_0).TargetA = SpellImplicitTargetInfo(TARGET_UNIT_TARGET_ENEMY);
+    });
+
     for (uint32 i = 0; i < GetSpellInfoStoreSize(); ++i)
     {
         SpellInfo* spellInfo = mSpellInfoMap[i];
