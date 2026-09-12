8cb495946ababedb8a5782a9379e236fbe9f6089
ccrs <ccrs@users.noreply.github.com>
2026-09-12 22:47:50 +0200

    Merge TrinityCore 3.3.5 5386f734a246868d9f74a9786cca10cf0c4a39d4

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -18033,7 +18033,7 @@ void Player::_LoadInventory(PreparedQueryResult result, uint32 timeDiff)
                 draft.AddItem(problematicItems.front());
                 problematicItems.pop_front();
             }
-            draft.SendMailTo(trans, this, MailSender(this, MAIL_STATIONERY_GM), MAIL_CHECK_MASK_COPIED);
+            draft.SendMailTo(trans, this, MailSender(this, MAIL_STATIONERY_GM), MAIL_CHECK_MASK_COPIED | MAIL_CHECK_MASK_NOT_RETURNABLE);
         }
         CharacterDatabase.CommitTransaction(trans);
     }
@@ -18245,7 +18245,7 @@ void Player::_LoadMail(PreparedQueryResult mailsResult, PreparedQueryResult mail
             m->deliver_time   = time_t(fields[7].GetUInt32());
             m->money          = fields[8].GetUInt32();
             m->COD            = fields[9].GetUInt32();
-            m->checked        = fields[10].GetUInt8();
+            m->checked        = fields[10].GetUInt32();
             m->stationery     = fields[11].GetUInt8();
             m->mailTemplateId = fields[12].GetInt16();
 
@@ -19677,7 +19677,7 @@ void Player::_SaveMail(CharacterDatabaseTransaction trans)
             stmt->setUInt32(2, uint32(m->deliver_time));
             stmt->setUInt32(3, m->money);
             stmt->setUInt32(4, m->COD);
-            stmt->setUInt8(5, uint8(m->checked));
+            stmt->setUInt32(5, uint8(m->checked));
             stmt->setUInt32(6, m->messageID);
 
             trans->Append(stmt);
@@ -23506,7 +23506,7 @@ void Player::AutoUnequipOffhandIfNeed(bool force /*= false*/)
         offItem->SaveToDB(trans);                                // recursive and not have transaction guard into self, item not in inventory and can be save standalone
 
         std::string subject = GetSession()->GetTrinityString(LANG_NOT_EQUIPPED_ITEM);
-        MailDraft(subject, "There were problems with equipping one or several items").AddItem(offItem).SendMailTo(trans, this, MailSender(this, MAIL_STATIONERY_GM), MAIL_CHECK_MASK_COPIED);
+        MailDraft(subject, "There were problems with equipping one or several items").AddItem(offItem).SendMailTo(trans, this, MailSender(this, MAIL_STATIONERY_GM), MAIL_CHECK_MASK_COPIED | MAIL_CHECK_MASK_NOT_RETURNABLE);
 
         CharacterDatabase.CommitTransaction(trans);
     }
